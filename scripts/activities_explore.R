3df297c413ded48e1d90e0863ed18c63b629e86b
Sam Langton <36928709+langtonhugh@users.noreply.github.com>
2024-08-24 16:28:58 +0200

    scaling up for all activities

--- scripts/activities_explore.R
+++ scripts/activities_explore.R
@@ -1,27 +1,45 @@
 # Load libraries.
+library(pbapply)
 library(XML)
 library(dplyr)
 library(lubridate)
 library(ggplot2)
 library(sf)
 
-# Parse a single gpx file.
-gpx <- htmlTreeParse(file = "data/4812281446.gpx", useInternalNodes = TRUE)
+# Create list of all the gpx files that we have.
+file_names <- paste0(
+  "data/",
+  list.files("data", pattern = glob2rx("*.gpx"))
+)
 
+# Read them all into a list.
+raw_list <- pblapply(file_names, function(x){
+  htmlTreeParse(file = x, useInternalNodes = TRUE)
+}
+)
+
+# How many activities do we have?
+length(raw_list)
+
+# Function for extracting the relevant information.
+acts_clean <- list()
+
+for (i in seq_along(raw_list)){
+  
 # Extract name.
-name <- xpathSApply(doc = gpx, path = "//trk/name", fun = xmlValue)
+name <- xpathSApply(doc = raw_list[[i]], path = "//trk/name", fun = xmlValue)
 
 # Extract type.
-type <- xpathSApply(doc = gpx, path = "//trk/type", fun = xmlValue)
+type <- xpathSApply(doc = raw_list[[i]], path = "//trk/type", fun = xmlValue)
 
 # Extract coords.
-coords <- xpathSApply(doc = gpx, path = "//trkpt", fun = xmlAttrs)
+coords <- xpathSApply(doc = raw_list[[i]], path = "//trkpt", fun = xmlAttrs)
 
 # Extract elevation.
-elevation <- xpathSApply(doc = gpx, path = "//trkpt/ele", fun = xmlValue)
+elevation <- xpathSApply(doc = raw_list[[i]], path = "//trkpt/ele", fun = xmlValue)
 
 # Extract time.
-time <- xpathSApply(doc = gpx, path = "//trkpt/time", fun = xmlValue)
+time <- xpathSApply(doc = raw_list[[i]], path = "//trkpt/time", fun = xmlValue)
 
 # Extract information into a dataframe.
 gpx_sf <- data.frame(
@@ -32,11 +50,68 @@ gpx_sf <- data.frame(
   lon         = coords["lon", ],
   ele         = as.numeric(elevation)
 ) %>% 
-  mutate(timestamps = ymd_hms(timestamps),
-         ele_r = round(ele/0.5)*0.5,   # round to nearest 0.5.
-         secs  = 1:nrow(.),            # create seconds from zero.
-         mins  = round(secs/60,0)) %>% # same but for minutes.
+  mutate(timestamps = ymd_hms(timestamps)) %>% 
   st_as_sf(coords = c(x = "lon", y = "lat"), crs = 4326) 
+
+# Insert each into the list.
+acts_clean[[i]] <- gpx_sf
+
+}
+
+# Bind together for broad summaries, then filter for runs only.
+acts_sf <- bind_rows(acts_clean, .id = "act_id") %>% 
+  filter(act_type == "running")
+
+# Convert coords to lines.
+acts_lines_sf <- acts_sf %>% 
+  group_by(act_id) %>% 
+  summarize(do_union=FALSE) %>% 
+  st_cast("LINESTRING") %>% 
+  ungroup() 
+
+# Create df of the distances.
+acts_dist_df <- acts_lines_sf %>% 
+  mutate(total_km = round(as.numeric(st_length(.)/1000), 2)) %>% 
+  as_tibble() %>% 
+  select(-geometry) 
+
+# Activity-level stats.
+stats_df <- acts_sf %>% 
+  as_tibble() %>% 
+  group_by(act_id) %>% 
+  summarize(
+  total_mins  = as.numeric(max(test$timestamps)-min(test$timestamps)),
+  ele_gain    = sum(diff(ele)[diff(ele) > 0])
+) %>% 
+  left_join(acts_dist_df) %>%
+  mutate(av_km_time = total_mins/total_km,
+         act_id     = as.numeric(act_id)) %>% 
+  mutate_if(is.numeric, function(x)round(x, 2))
+
+# Distribution of distances.
+ggplot(data = stats_df) +
+  geom_histogram(mapping = aes(x = total_km), bins = 16, fill = "#fc4c02") +
+  theme_minimal() +
+  labs(y = NULL, x = "Km")
+
+# Distribution of time running.
+ggplot(data = stats_df) +
+  geom_histogram(mapping = aes(x = total_mins), bins = 20, fill = "#fc4c02") +
+  theme_minimal() +
+  labs(y = NULL, x = "Km")
+
+# Distribution of speed.
+ggplot(data = stats_df) +
+  geom_histogram(mapping = aes(x = av_km_time), bins = 30, fill = "#fc4c02") +
+  theme_minimal() +
+  labs(y = NULL, x = "Km")
+
+# Distribution of elevation gains.
+ggplot(data = stats_df) +
+  geom_histogram(mapping = aes(x = ele_gain), bins = 20, fill = "#fc4c02") +
+  theme_minimal() +
+  labs(y = NULL, x = "Km")
+
 
 # Single activity elevation.
 ggplot(data = gpx_sf) +
@@ -56,15 +131,4 @@ ggplot(data = gpx_line_sf) +
   geom_sf(colour = "#fc4c02", linewidth = 1) +
   theme_minimal()
 
-# Activity-level stats.
-stats_df <- data.frame(
-  act_name    = name,
-  act_type    = type,
-  total_mins  = as.numeric(max(gpx_sf$timestamps)-min(gpx_sf$timestamps)),
-  total_km    = as.numeric(st_length(gpx_line_sf)/1000),
-  ele_gain    = sum(diff(gpx_sf$ele)[diff(gpx_sf$ele) > 0])
-) %>% 
-  mutate(
-    av_km_time     = total_mins/total_km
-  ) %>% 
-  mutate_if(is.numeric, function(x)round(x, 2))
+
